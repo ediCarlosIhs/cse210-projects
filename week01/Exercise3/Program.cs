03594b94c986724c6560521ae1c99ed2c3bcb6f1
Edi Carlos <edi_csilva@hotmail.com>
2026-06-30 11:31:12 -0300

    first requirement of the exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        System.Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+
+
+        string shot = string.Empty;
+
+        do
+        {
+            System.Console.Write("What is your guess? ");
+            shot = Console.ReadLine();
+
+            if (int.Parse(magicNumber) > int.Parse(shot))
+            {
+                System.Console.WriteLine("Higher");
+            }
+            else if (int.Parse(magicNumber) < int.Parse(shot))
+            {
+                System.Console.WriteLine("Lower");
+            }
+            else
+            {
+                System.Console.WriteLine("You guessed it!");
+            }
+        }
+        while (int.Parse(magicNumber) != int.Parse(shot));
+
+
+
     }
 }
